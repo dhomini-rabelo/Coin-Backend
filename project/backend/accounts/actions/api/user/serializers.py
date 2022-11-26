3361bdc222ce95bcfe65a0ee28b704025e956bdd
Dhomini  Rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-25 23:48:54 -0300

    add user serializer

---
+++ project/backend/accounts/actions/api/user/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from backend.accounts.actions.api.user.typings import CreateUserRequestBodyType, CreateUserValidatedDataType
+from backend.accounts.app.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance: User):
+        return {
+            "username": instance.username,
+        }
+    
+    def validate(self, validated_data: CreateUserValidatedDataType):
+        initial_data: CreateUserRequestBodyType = self.initial_data
+        if not initial_data.get('confirm_password'):
+            raise serializers.ValidationError({'confirm_password': 'Este campo é obrigatório'})
+        elif initial_data['confirm_password'] != validated_data['password']:
+            raise serializers.ValidationError({'confirm_password': 'As senhas são diferentes'})
+        return validated_data        
+
+    def create(self, validated_data: CreateUserValidatedDataType):
+        new_user = User(username=validated_data['username'])
+        new_user.set_password(validated_data['password'])
+        new_user.save()
+        return new_user
+
+    class Meta:
+        model = User
+        fields = 'username', 'password',
---
+++ project/backend/accounts/actions/api/user/typings.py
@@ -0,0 +1,12 @@
+from typing import TypedDict
+
+
+class CreateUserRequestBodyType(TypedDict):
+    username: str | None
+    password: str | None
+    confirm_password: str | None
+
+
+class CreateUserValidatedDataType(TypedDict):
+    username: str
+    password: str
