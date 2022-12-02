8926595d1960d70e936366e83ff4910d912fb9d8
Dhomini  Rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-01 23:23:07 -0300

    add notification_time field

--- project/backend/accounts/app/models.py
+++ project/backend/accounts/app/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.utils.safestring import mark_safe
 from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField, SET_NULL, CASCADE)
+from backend.accounts.app.support.models.choices import NOTIFICATION_TIME_CHOICES
+
 
 
 class User(AbstractUser):
+    notification_time = CharField(max_length=64, choices=NOTIFICATION_TIME_CHOICES, default=NOTIFICATION_TIME_CHOICES[0][0])
 
     def __str__(self):
         return self.username
