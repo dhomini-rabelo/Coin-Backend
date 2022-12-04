20714a2c3aba3c12d072a4b3c1a5d120d0ddc1cc
Dhomini  Rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-04 16:28:55 -0300

    validate bill in serializer

--- project/backend/bills/actions/api/bills/serializers.py
+++ project/backend/bills/actions/api/bills/serializers.py
@@ -6,9 +6,22 @@ from backend.bills.app.models import Bill
 class BillSerializer(serializers.ModelSerializer):
 
     def validate_day(self, value: str | int):
-        if not (str(value).isnumeric() and (int(value) in range(1, 28))):
+        if str(value).isnumeric() and (int(value) not in range(1, 28)):
             raise serializers.ValidationError('Valor inválido')
         return value
+
+    def validate(self, validated_data: dict):
+        if validated_data.get('bill_type') in ['scheduled_income', 'scheduled_expense']:
+            if not validated_data.get('day'):
+                raise serializers.ValidationError({'day': ['Este campo é obrigatório']})
+            elif not validated_data.get('partials'):
+                raise serializers.ValidationError({'partials': ['Este campo é obrigatório']})
+        else:
+            if validated_data.get('day') is not None:
+                raise serializers.ValidationError({'day': ['Este campo é inválido']})
+            elif validated_data.get('partials') is not None:
+                raise serializers.ValidationError({'partials': ['Este campo é inválido']})
+        return validated_data
 
     class Meta:
         model = Bill
