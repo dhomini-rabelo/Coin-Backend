f773256b638e26cee79c7ea93f79094de38b8ceb
Dhomini  Rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-11 16:12:50 -0300

    use private and readonly in CacheController

--- project/Core/controllers/cache.py
+++ project/Core/controllers/cache.py
@@ -6,30 +6,33 @@ from typing import Any
 class CacheController:
 
     def __init__(self, cache_name: str):
-        self.name = cache_name
+        self._cache_name = cache_name
 
-    def get(self, request: HttpRequest, id_for_cache: str | None = None) -> Any:
-        print('cache -> get: ', self.get_cache_name_for_save(request.path, id_for_cache))
-        return cache.get(self.get_cache_name_for_save(request.path, id_for_cache))
+    @property
+    def cache_name(self): return self._cache_name
+
+    def _get(self, request: HttpRequest, id_for_cache: str | None = None) -> Any:
+        print('cache -> get: ', self._get_cache_name_for_save(request.path, id_for_cache))
+        return cache.get(self._get_cache_name_for_save(request.path, id_for_cache))
 
     def has_cache(self, request: HttpRequest, id_for_cache: str | None = None) -> has_cache_response_type:
-        saved_cache = self.get(request, id_for_cache)
-        print('cache -> has_cache: ', self.get_cache_name_for_save(request.path, id_for_cache), saved_cache is not  None)
+        saved_cache = self._get(request, id_for_cache)
+        print('cache -> has_cache: ', self._get_cache_name_for_save(request.path, id_for_cache), saved_cache is not  None)
         return {
             "response": saved_cache is not None,
             "cache_data": saved_cache,
         }
 
-    def get_cache_name_for_save(self, url: str, id_for_cache: str | None = None) -> str:
-        return f'{self.name}-{url}-{id_for_cache}' if id_for_cache else f'{self.name}-{url}'
+    def _get_cache_name_for_save(self, url: str, id_for_cache: str | None = None) -> str:
+        return f'{self._cache_name}-{url}-{id_for_cache}' if id_for_cache else f'{self._cache_name}-{url}'
 
     def save(self, data: Any, request: HttpRequest, id_for_cache: str | None = None):
-        cache_name_for_save = self.get_cache_name_for_save(request.path, id_for_cache)
+        cache_name_for_save = self._get_cache_name_for_save(request.path, id_for_cache)
         print('cache -> save: ', cache_name_for_save, data)
         cache.set(cache_name_for_save, data, None)
 
     def delete_only(self, url: str, cache_id: str):
-        cache_name_for_delete = self.get_cache_name_for_save(url, cache_id)
+        cache_name_for_delete = self._get_cache_name_for_save(url, cache_id)
         print('cache -> delete_only: ', cache_name_for_delete)
         cache.delete(cache_name_for_delete) 
 
@@ -42,7 +45,7 @@ class CacheController:
 
         all_cache_keys_as_binary_string_and_with_client_number = cache._cache.get_client().keys('*')
         all_cache_keys = [clear_key(key) for key in all_cache_keys_as_binary_string_and_with_client_number]
-        keys_for_delete = [key for key in all_cache_keys if key.startswith(self.name)]
+        keys_for_delete = [key for key in all_cache_keys if key.startswith(self._cache_name)]
         cache.delete_many(keys_for_delete)
 
 
